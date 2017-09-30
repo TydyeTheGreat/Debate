6049cf5d51b53ed6e8cb015af4f9383705ce93d7
tydyethegreat <ssdralian@gmail.com>
2017-09-30 16:58:13 +0000

    second commit

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ $(".signin").on("click", function(){
     console.log(email, password);
     firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
          console.log("error");
-         
+         document.location = "https://preview.c9users.io/tydyethegreat/signup/index.html?_c9_id=livepreview0&_c9_host=https://ide.c9.io";
+        
     });
         
 });
